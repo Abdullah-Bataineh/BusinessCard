60f737d42cd4fd03d95f65a5b0d1548cccc639ae
Abdullah-Bataineh <78338087+Abdullah-Bataineh@users.noreply.github.com>
2024-10-09 15:46:58 +0300

    Add Complete

--- BusinessCard Service/BusinessCardApi/BusinessCardApi/obj/Debug/net7.0/BusinessCardApi.AssemblyInfo.cs
+++ BusinessCard Service/BusinessCardApi/BusinessCardApi/obj/Debug/net7.0/BusinessCardApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BusinessCardApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0b3dec930dff4dfb396ad061b923d5ccc65eb29")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4569aa7afd42d36c213bba68e728fbcafaf33898")]
 [assembly: System.Reflection.AssemblyProductAttribute("BusinessCardApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BusinessCardApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
