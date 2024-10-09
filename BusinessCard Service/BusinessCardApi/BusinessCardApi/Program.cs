d38eb9f268068949d166149a9f946cc597f1e065
Abdullah-Bataineh <78338087+Abdullah-Bataineh@users.noreply.github.com>
2024-10-09 17:03:56 +0300

    BuildProgram with Add scopes and dependency Injection

--- BusinessCard Service/BusinessCardApi/BusinessCardApi/Program.cs
+++ BusinessCard Service/BusinessCardApi/BusinessCardApi/Program.cs
@@ -1,3 +1,8 @@
+using BusinessCardApi.AppDatabaseContext;
+using BusinessCardApi.Repository;
+using BusinessCardApi.Service;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IBusinessCardRepository,BusinessCardRepository>();
+builder.Services.AddScoped<IBusinessCardService,BusinessCardService>();
 
 var app = builder.Build();
 
