7f31a8f3978aaf4c0a7e8ffd0997b0ca9139c479
Abdullah-Bataineh <78338087+Abdullah-Bataineh@users.noreply.github.com>
2024-10-11 02:49:57 +0300

    update model

--- BusinessCard Service/BusinessCardApi/BusinessCardApi/Migrations/20241010182934_ThirdCreate.Designer.cs
+++ BusinessCard Service/BusinessCardApi/BusinessCardApi/Migrations/20241010182934_ThirdCreate.Designer.cs
@@ -4,6 +4,7 @@ using BusinessCardApi.AppDatabaseContext;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BusinessCardApi.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241010182934_ThirdCreate")]
+    partial class ThirdCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
